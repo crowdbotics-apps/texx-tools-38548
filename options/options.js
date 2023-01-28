3769d7652a3ca593262ec89f79ab8f53e31e576c
Crowdbotics <team@crowdbotics.com>
2023-01-28 18:52:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "texx-tools-38548",
+  url: "https://texx-tools-38548.botics.co",
+  api: "https://texx-tools-38548.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
